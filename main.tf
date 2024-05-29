3f299e8492b7d725a58182423dac02c19a1c34fb
Anastasia Maidanyuk <maidanyuk.2004@gmail.com>
2024-05-29 22:07:24 +0300

    add commit

---
+++ main.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.aws_region}a"
+}
+
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["195.26.18.182/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app" {
+  ami            = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.main.id
+  security_groups = [aws_security_group.main.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+              sudo apt-get update
+              sudo apt-get install -y docker-ce
+              sudo usermod -aG docker ubuntu
+              sudo systemctl start docker
+              sudo systemctl enable docker
+      sudo docker pull maidanyuk/lab5
+      cd lab5
+      sudo docker run -d -p 80:80 maidanyuk/lab5:latest
+      docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --interval 60
+              EOF
+
+  tags = {
+    Name = "js-app-instance"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.app.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.app.public_ip
+}
